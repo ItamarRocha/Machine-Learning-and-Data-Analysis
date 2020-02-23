6995f8cccea2460f153661833b5e22e6c0e52e3a
itamar <itamardprf@gmail.com>
2020-02-23 16:25:23 -0300

    advances

---
+++ live sellers/livesellers.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 22 21:30:16 2020
+
+@author: itamar
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime as dt
+
+dataset = pd.read_csv("Live.csv").drop(columns = {'status_id','Column1','Column2','Column3','Column4'})
+dataset['status_type'].value_counts()
+dataset['status_published'] = dataset['status_published'].astype(str).str.replace("/","-")
+dataset['status_published'] = pd.to_datetime(dataset['status_published'])
+
+dataset['day'] = dataset['status_published'].dt.day
+dataset['month'] = dataset['status_published'].dt.month
+dataset['hour'] = dataset['status_published'].dt.hour
+dataset['minute'] = dataset['status_published'].dt.minute
+
+from sklearn.preprocessing import LabelEncoder
+encoder = LabelEncoder()
+dataset['status_type'] = encoder.fit_transform(dataset['status_type'])
+
+dataset = dataset.drop(columns = {'status_published'})
+
+"""
+def plot_dendrogram(model, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
+
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack([model.children_, model.distances_,
+                                      counts]).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
+
+
+# setting distance_threshold=0 ensures we compute the full tree.
+model = AgglomerativeClustering(distance_threshold=0, n_clusters=None)
+
+model = model.fit(dataset)
+plt.title('Hierarchical Clustering Dendrogram')
+# plot the top three levels of the dendrogram
+plot_dendrogram(model, truncate_mode='level', p=3)
+plt.xlabel("Number of points in node (or index of point if no parenthesis).")
+plt.show()
+"""
+X = dataset
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+    kmeans.fit(dataset)
+    wcss.append(kmeans.inertia_)
+plt.figure(1)
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+#we dont choose random to dont fall in the random trap
+kmeans = KMeans(n_clusters = 5, init = 'k-means++', random_state = 42)
+y_kmeans = kmeans.fit_predict(X)
+
+pd.DataFrame(y_kmeans)[0].value_counts()
+
+ 
+
+# Using the dendrogram to find the optimal number of clusters
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'complete'))
+#plt.figure(2)
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
+ 
+# Fitting Hierarchical Clustering to the dataset
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'complete')
+y_hc = hc.fit_predict(X)
+
+pd.DataFrame(y_hc)[0].value_counts()
+
+
+
+ 
+
+# Using the dendrogram to find the optimal number of clusters
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'single'))
+#plt.figure(2)
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
+ 
+# Fitting Hierarchical Clustering to the dataset
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'single')
+y_hc = hc.fit_predict(X)
+
+pd.DataFrame(y_hc)[0].value_counts()
+
+
+
+
+
+
+# Using the dendrogram to find the optimal number of clusters
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+#plt.figure(2)
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
+ 
+# Fitting Hierarchical Clustering to the dataset
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'ward')
+y_hc = hc.fit_predict(X)
+
+pd.DataFrame(y_hc)[0].value_counts()
+
+
+
+# Using the dendrogram to find the optimal number of clusters
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'average'))
+#plt.figure(2)
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
+ 
+# Fitting Hierarchical Clustering to the dataset
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'average')
+y_hc = hc.fit_predict(X)
+
+pd.DataFrame(y_hc)[0].value_counts()
--- metro/metro.py
+++ metro/metro.py
@@ -11,6 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
 
 df = pd.read_csv("Metro_Interstate_Traffic_Volume.csv")
 
@@ -21,12 +22,27 @@ df['hour'] = (pd.to_datetime(df['date_time']).dt.hour).astype(int)
 df.drop(columns = ['date_time'],inplace = True)
 
 df.isnull().sum()
-df.duplicated()
+df.duplicated().sum()
+df = df.drop_duplicates()
 
 stats = df.describe(include = "all")
 
 encoder = LabelEncoder()
 df['holiday'].loc[df['holiday'] == 'None'] = str(0)
+loc = df['temp'].loc[df['temp'] == 0].index
+print(loc)
+value = loc[0]
+lol = df['temp'].iloc[loc[0]-2:loc[3]+2]
+df['temp'].loc[df['temp'] == 0] = np.nan
+df ['temp'] = df['temp'].fillna(method = 'ffill')
+
+df['rain_1h'].loc[df['rain_1h'] == 9831.0] = np.nan
+print(df.isnull().sum())
+df ['rain_1h'] = df['rain_1h'].fillna(method = 'ffill')
+print(df.isnull().sum())
+
+
+
 #df['holiday'].loc[df['holiday'] != 0] = 1
 """
 na minha cabeça, se eu boto o dia a hora e o mes, dizer o feriado nao faz sentido
@@ -35,3 +51,9 @@ só botar 0 ou 1 já é suficientye
 df['holiday'] = encoder.fit_transform(df['holiday'])
 df['weather_main'] = encoder.fit_transform(df['weather_main'])
 df['weather_description'] = encoder.fit_transform(df['weather_description'])
+
+
+transformer = ColumnTransformer(transformers=[("OneHot",OneHotEncoder(),[5,6])],remainder='passthrough')
+df = transformer.fit_transform(np.asarray(df).tolist())
+df = df.astype('float64')
+df = pd.DataFrame(df)
